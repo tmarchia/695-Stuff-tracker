74372c3de9a2d30cd9fa05da35eecebf8dbaeeca
kunalsatija009 <83098182+kunalsatija009@users.noreply.github.com>
2022-03-21 13:15:45 -0400

    Add files via upload
    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask,render_template
+app=Flask(__name__,template_folder='Templates')
+@app.route("/")
+def home():
+    # return "<h1>hello kunal</h1>"
+    return render_template("index.html")
+if __name__ =="__main__":
+    app.run(debug=True)
