fbbaa5f9ea7389c00d833b0eac5cd890e62e73be
kunalsatija009 <83098182+kunalsatija009@users.noreply.github.com>
2022-03-21 17:42:15 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask,render_template
+from logging import error
+from flask import Flask,render_template,session,request
 app=Flask(__name__,template_folder='Templates')
-@app.route("/")
-def home():
-    # return "<h1>hello kunal</h1>"
+app.secret_key = "mykey"
+
+
+@app.route("/" ,methods =['GET','POST'])
+def welcome():
+    error = None
+    if request.method == 'POST':
+        if request.form ['username'] != 'kunal' or request.form['password']!= 'kunal@123':
+            error = "Please insert valid details"
+        else:
+            session['logged_in']= True
+            return render_template("about.html")
+    return render_template("index.html" , error = error)
+
+
+@app.route("/about")
+def about ():
     return render_template("index.html")
+
+@app.route("/logout")
+def logout():
+    session.pop('logged_in' ,None)
+    return render_template("index.html")
+
 if __name__ =="__main__":
     app.run(debug=True)
