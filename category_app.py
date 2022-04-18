96f2431b86e7aff4caaad127ef7507682bf3bc15
tmarchia <74574315+tmarchia@users.noreply.github.com>
2022-04-18 15:54:40 -0400

    update category_app.py to include updates for certificate
    add functionality to use a custom certificate. This will allow the website to show as secure

--- category_app.py
+++ category_app.py
@@ -153,4 +153,4 @@ def get_user_name(code):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, ssl_context='adhoc')
+    app.run(host="0.0.0.0", port=5000, ssl_context=('fullchain.pem', 'privkey.pem'))
